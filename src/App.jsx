34a36ea18dcbcedc063207cac55364c856528cb9
James <jfnelson2000@gmail.com>
2023-04-27 20:47:40 -0700

    components developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
 import { DataContext } from './Context/DataContext';
 import { SearchContext } from './Context/SearchContext';
+import AlbumView from './Components/AlbumView';
+import ArtistView from './Components/ArtistView'
 
 function App() {
   const [data, setData] = useState([])
@@ -29,16 +32,26 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
-      {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar />
+              </SearchContext.Provider>
+              {message}
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path='/album/:id' element={<AlbumView />} />
+          <Route path='/artist/:id' element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
