63fba424bd52d2e8d4ca00e29b1e14b0e2354ee5
James <jfnelson2000@gmail.com>
2023-04-27 18:25:38 -0700

    API fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
@@ -7,6 +7,20 @@ function App() {
   const [search, setSearch] = useState("")
   const [data, setData] = useState([])
   const [message, setMessage] = useState("Search for Music!")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`
+      const response = await fetch('https://itunes.apple.com/search?term=nirvana')
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Artist not found!')
+      }
+    }
+    fetchData()
+  }, [search])
 
   return (
     <div className="App">
