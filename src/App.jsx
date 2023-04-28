fb7877cd4c363e98a0839d70b228e0f7ab36629c
James <jfnelson2000@gmail.com>
2023-04-27 18:07:14 -0700

    search bar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [data, setData] = useState([])
+  const [message, setMessage] = useState("Search for Music!")
+
   return (
     <div className="App">
       <SearchBar />
+      {message}
       <Gallery />
     </div>
   );
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
+
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState("")
+
     return (
-        <h1>SearchBar</h1>
+        <form>
+            <input type="text" placeholder="Enter a search term here" />
+            <input type="submit" />
+        </form>
     )
 }
 
