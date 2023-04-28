7098ee5879af094f96b410bbefe0ca15f6390f17
James <jfnelson2000@gmail.com>
2023-04-27 20:08:23 -0700

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
@@ -6,32 +6,25 @@ import { DataContext } from './Context/DataContext';
 import { SearchContext } from './Context/SearchContext';
 
 function App() {
-  const [search, setSearch] = useState("")
   const [data, setData] = useState([])
   const [message, setMessage] = useState("Search for Music!")
   let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Artist not found!')
-        }
-      }
-      fetchData()
-    }
-  }, [search])
-
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearch(term)
+    const fetchData = async () => {
+      document.title = `${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Artist not found!')
+      }
+    }
+    fetchData()
   }
 
   return (
